8381cfb63153aa45ce5c5b082a0b35d7dca239bf
VolodAs <vladimiraseev89@gmail.com>
2026-04-26 13:00:49 +0300

    add change theme

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import {
   handlerInitPage,
   handleRemoveTask,
 } from './js/tasks';
+import { changeTheme } from './js/theme-switcher';
 
 document.addEventListener('DOMContentLoaded', handlerInitPage);
 
 refs.form.addEventListener('submit', handelSubmitTask);
 refs.list.addEventListener('click', handleRemoveTask);
+refs.themeToggle.addEventListener('click', changeTheme);
